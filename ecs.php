0c23b721c8f188afa7fcd1536044e160be41d1fc
Sam Mousa <sam@mousa.nl>
2023-08-23 15:46:33 +0200

    feat: virtual directory adapter with support for metadata

--- ecs.php
+++ ecs.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 // ecs.php
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+return static function (ECSConfig $ECSConfig): void {
+    $parameters = $ECSConfig->parameters();
     // Parallel
     $parameters->set(Option::PARALLEL, true);
 
@@ -21,11 +21,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     ]);
     // A. full sets
-    $containerConfigurator->import(SetList::PSR_12);
+    $ECSConfig->import(SetList::PSR_12);
 
 
     // B. standalone rule
-    $services = $containerConfigurator->services();
+    $services = $ECSConfig->services();
     $services->set(ArraySyntaxFixer::class)
         ->call('configure', [[
             'syntax' => 'short',
---
+++ tests/VirtualDirectoryListWithMetaDataAdapterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\FlySystem\Tests;
+
+use Collecthor\FlySystem\VirtualDirectoryListWithMetaDataAdapter;
+use League\Flysystem\Config;
+use League\Flysystem\DirectoryAttributes;
+use League\Flysystem\FileAttributes;
+use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
+
+/**
+ * @covers \Collecthor\FlySystem\VirtualDirectoryListWithMetaDataAdapter
+ * @uses \Collecthor\FlySystem\IndirectAdapter
+ */
+class VirtualDirectoryListWithMetaDataAdapterTest extends IndirectAdapterTestCase
+{
+    public static function clearFilesystemAdapterCache(): void
+    {
+        parent::clearFilesystemAdapterCache();
+    }
+
+
+    protected static function createFilesystemAdapter(): VirtualDirectoryListWithMetaDataAdapter
+    {
+        $memoryAdapter = new InMemoryFilesystemAdapter();
+        $memoryAdapter->createDirectory('test123', new Config());
+        return new VirtualDirectoryListWithMetaDataAdapter($memoryAdapter, 'test123/', [
+            'abc' => ['meta' => 'cool'],
+            'def' => ['meta' => 'stuff'],
+        ]);
+    }
+
+
+    public function testListVirtualDirectoriesWhenPrimaryIsEmpty(): void
+    {
+        $adapter = $this->adapter();
+        $this->assertListingLength(3, $adapter, 'test123/', false);
+        $this->assertListingsAreTheSame([
+            new DirectoryAttributes('test123'),
+            new DirectoryAttributes('test123/abc'),
+            new DirectoryAttributes('test123/def')
+        ], $adapter->listContents('test123', false));
+    }
+
+    public function testListVirtualDirectoriesPrimaryNonEmpty(): void
+    {
+        $adapter = $this->adapter();
+        $this->assertListingLength(3, $adapter, 'test123/', false);
+        $this->assertListingLength(3, $adapter, 'test123/', true);
+
+        $this->givenWeHaveAnExistingFile('test123/abc/test', 'cool');
+        $this->assertListingLength(3, $adapter, 'test123/', false);
+        $this->assertListingLength(4, $adapter, 'test123/', true);
+
+        $this->assertListingsAreTheSame([
+            new DirectoryAttributes('test123'),
+            new DirectoryAttributes('test123/abc'),
+            new DirectoryAttributes('test123/def')
+        ], $adapter->listContents('test123', false));
+
+        $this->assertListingsAreTheSame([
+            new DirectoryAttributes('test123'),
+            new DirectoryAttributes('test123/abc'),
+            new DirectoryAttributes('test123/def'),
+            new FileAttributes('test123/abc/test'),
+
+        ], $adapter->listContents('test123', true));
+    }
+
+    public function testListVirtualDirectoriesDeep(): void
+    {
+        $adapter = $this->adapter();
+        $this->assertListingLength(3, $adapter, '', true);
+
+
+        $this->assertListingsAreTheSame([
+            new DirectoryAttributes('test123'),
+            new DirectoryAttributes('test123/abc'),
+            new DirectoryAttributes('test123/def')
+        ], $adapter->listContents('test123', true));
+    }
+}
